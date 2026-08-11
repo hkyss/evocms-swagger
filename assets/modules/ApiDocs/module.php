bbab146d7db12cd5a364dd8680faa8c9ac625091
Vladislav <bendik@qmedia.by>
2026-08-11 15:19:25 +0300

    fix module index.php path

--- assets/modules/ApiDocs/module.php
+++ assets/modules/ApiDocs/module.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-$index = __DIR__ . '/index.php';
+$index = MODX_BASE_PATH . 'core/vendor/vvvladv/evo-swagger/assets/modules/ApiDocs/index.php';
 
 if (!is_file($index)) {
     echo '<div class="api-docs-error" role="alert"><strong>evo-swagger</strong>: не найден index.php модуля.</div>';
